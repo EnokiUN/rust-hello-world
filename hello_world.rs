cfaab886e90732567f48e5e4db8d49dbaf1bb4c2
Enoki <enokiun@gmail.com>
2022-04-10 00:51:08 +0300

    Update hello_world.rs

--- hello_world.rs
+++ hello_world.rs
@@ -1,3 +1,10 @@
+// wtf why are you looking at my code who looks at other people's code ewwww
+// real installation instructions:
+// either clone the repo or wget this file using raw github thingy idfk
+// compile using rustc or whatever unholy / holy method of compiling you use.
+// run `./hello_world`
+// congrats you just ran a :rocket:b:rocket:l:rocket:a:rocket:z:rocket:i:rocket:n:rocket:g:rocket:l:rocket:y:rocket: :rocket:f:rocket:a:rocket:s:rocket:t:rocket:!:rocket:!:rocket:!:rocket: implementation of the classical Hello World program in rust.
+// Oh, you don't use linux? sucks to su-
 fn main(){
     println!("Hello, World!");
 } 
